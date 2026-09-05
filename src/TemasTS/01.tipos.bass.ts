a149e7bba00d8b812845c7b6aa5ccbbfa4e27e4b
JnEsteban <stebanutlevn@gmail.com>
2026-09-04 18:56:36 -0600

    Tipo De Datos 4/9/2026

---
+++ src/TemasTS/01.tipos.bass.ts
@@ -0,0 +1,17 @@
+console.log("Hola Mundo n_n");
+
+var num1 = 39;
+let num2 = 61;
+const num3 = 300;
+
+let nombre: string;
+nombre = "Pedro";
+
+let edad: number;
+let isMayorEdad: boolean;
+
+let sum: number=num1+num2+num3;
+console.log("La suma es: "+sum);
+
+console.log(nombre)
+
